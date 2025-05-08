f02bb2e71ec12e37fc1f375e856a1e3b43780bc9
RavindraSSK <64724783+RavindraSSK@users.noreply.github.com>
2025-05-08 01:37:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ uploaded_file = st.file_uploader("📷 Upload an image", type=["jpg", "jpeg", "p
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # ✨ Step 1: Generate image caption using BLIP
     st.info("Generating image caption...")
